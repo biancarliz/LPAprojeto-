6e41786c336cc4f06ba42c8161813276a86db1fa
unknown <bianca-rliz@hotmail.com>
2024-03-22 22:43:37 -0300

    Signed-off-by: unknown <bianca-rliz@hotmail.com>

---
+++ Model/Level.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+        self.entity_list = None
+        self.name = None
+
+    def Operation1(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
